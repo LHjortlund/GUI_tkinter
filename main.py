b0d1d2196e9570849b5b6ca6c44bb49abdcad163
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-09-18 13:15:18 +0200

    added extra button and grid

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 import tkinter
 
-window = tkinter.Tk()
-window.title("My first GUI program")
-window.minsize(500, 500)
-
-#create a label
-my_label = tkinter.Label(text= "I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-#overwrite exciting label
-my_label["text"] = "New Text"
-my_label.config(text = "New Text")
-
 #create a funktion to the button
 #make the input the new text
 def button_cliked():
@@ -19,15 +7,31 @@ def button_cliked():
     new_text = input.get()
     my_label.config(text = new_text)
 
+window = tkinter.Tk()
+window.title("My first GUI program")
+window.minsize(500, 500)
+window.config(padx=20, pady=20) #you can add space around your widgets so it has some space
+
+#create a label
+my_label = tkinter.Label(text= "I am a label", font=("Arial", 24, "bold"))
+my_label.grid(column=0, row=0)
+
+#overwrite exciting label
+my_label["text"] = "New Text"
+my_label.config(text = "New Text")
+
+#new button
+new_button = tkinter.Button(text="New button")
+new_button.grid(column=2, row=0)
+
 #create a button
 button = tkinter.Button(text="Click Me!", command=button_cliked)
-button.pack()
+button.grid(column=1, row=1)
 
-#entry component
+#Entry component
 input = tkinter.Entry(width=10)
-input.pack()
 print(input.get())
-
+input.grid(column=3, row=3)
 
 
 
