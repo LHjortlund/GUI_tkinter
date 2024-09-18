b63d836ee4f97cedd5df82df5751382c48d30e32
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-09-18 09:40:30 +0200

    add button and function

--- main.py
+++ main.py
@@ -6,7 +6,21 @@ window.minsize(500, 500)
 
 #create a label
 my_label = tkinter.Label(text= "I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label.pack()
+
+#overwrite exciting label
+my_label["text"] = "New Text"
+my_label.config(text = "New Text")
+
+#create a funktion to the button
+def button_cliked():
+    print("Button clicked")
+
+#create a button
+button = tkinter.Button(text="Click Me!", command=button_cliked)
+button.pack()
+
+
 
 
 window.mainloop()
